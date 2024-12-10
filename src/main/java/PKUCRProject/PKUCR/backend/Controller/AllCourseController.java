f8da4a53d9898554f136451435bf7e302c252ad5
lvhangzhou <lvhangzhou503@126.com>
2024-12-10 09:49:01 +0800

    feat: get the database schema.sql worked.

--- src/main/java/PKUCRProject/PKUCR/backend/Controller/AllCourseController.java
+++ src/main/java/PKUCRProject/PKUCR/backend/Controller/AllCourseController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/PKUCRProject/PKUCR/backend/Controller/MaterialController.java
+++ src/main/java/PKUCRProject/PKUCR/backend/Controller/MaterialController.java
@@ -15,10 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.multipart.MultipartFile;
@@ -32,7 +30,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 import PKUCRProject.PKUCR.backend.Service.MaterialService;
 import PKUCRProject.PKUCR.backend.Entity.Material;
-import PKUCRProject.PKUCR.backend.Entity.User;
 import PKUCRProject.PKUCR.backend.Service.CustomUserDetailsService;
 
 @Tag(name = "MaterialController")
