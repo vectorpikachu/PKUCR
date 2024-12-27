d44f613e25a17472a8d382a18c80f55b14994ab1
Sunnycookies <15906275115@163.com>
2024-12-27 20:32:06 +0800

    Fix: modify server base url in axios.ts

--- src/axios.ts
+++ src/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { storage } from './store/storage'
 
 const instance = axios.create({
-	baseURL: 'http://localhost:38083', // 服务端基地址, 用https协议
+	baseURL: 'http://10.129.242.68:38083', // 服务端基地址, 用https协议
 })
 
 instance.interceptors.request.use(config => {
